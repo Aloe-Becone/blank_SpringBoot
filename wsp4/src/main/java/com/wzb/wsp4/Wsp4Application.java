f6a5cbd09eacd9235206d32aed950fd8bfd75573
Aloe-Becone <97663227+Aloe-Becone@users.noreply.github.com>
2023-11-12 19:39:36 +0800

    Add files via upload

---
+++ wsp4/src/main/java/com/wzb/wsp4/Wsp4Application.java
@@ -0,0 +1,14 @@
+package com.wzb.wsp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wsp4Application
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wsp4Application.class, args);
+	}
+
+}
